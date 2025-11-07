869129bb79c218d2eefd78040565568c26487c0e
Korolenko <korolenkoalexey84@gmail.com>
2025-11-07 17:38:54 +0200

    components create

---
+++ components/StoryCard/StoryCard.tsx
@@ -0,0 +1,9 @@
+export default async function renderStoryCard() {
+    return (
+        <section className="section">
+            <div className="container">
+                <h2>StoryCard</h2>
+            </div>
+        </section>
+    );
+}
---
+++ components/StoryCardsList/StoryCardsList.tsx
@@ -0,0 +1,9 @@
+export default async function renderStoryCardList() {
+    return (
+        <section className="section">
+            <div className="container">
+                <h2>StoryCardList</h2>
+            </div>
+        </section>
+    );
+}
