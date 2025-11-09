be721a34ab5e7339a9abdf31bbdd0d474ef98f80
Henry <hennadii.lytvyn@gmail.com>
2025-11-09 13:25:06 +0000

    social links add

--- components/SocialsList/SocialsList.tsx
+++ components/SocialsList/SocialsList.tsx
@@ -6,22 +6,34 @@ export default function SocialsList() {
   return (
     <ul className={css.list}>
       <li className={css.item}>
-        <Link className={css.link} href="#">
+        <Link
+          className={css.link}
+          href="https://www.facebook.com/"
+          target="_blank"
+        >
           <Icon name="icon-Facebook" className={css.icon}></Icon>
         </Link>
       </li>
       <li className={css.item}>
-        <Link className={css.link} href="#">
+        <Link
+          className={css.link}
+          href="https://www.instagram.com/"
+          target="_blank"
+        >
           <Icon name="icon-Instagram" className={css.icon}></Icon>
         </Link>
       </li>
       <li className={css.item}>
-        <Link className={css.link} href="#">
+        <Link className={css.link} href="https://x.com/" target="_blank">
           <Icon name="icon-X" className={css.icon}></Icon>
         </Link>
       </li>
       <li className={css.item}>
-        <Link className={css.link} href="#">
+        <Link
+          className={css.link}
+          href="https://www.youtube.com/"
+          target="_blank"
+        >
           <Icon name="icon-Youtube" className={css.icon}></Icon>
         </Link>
       </li>
